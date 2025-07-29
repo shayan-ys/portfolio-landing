9e8fa57ef74b69c5637fef484cf88618e031cf95
Shayan Ys <shayanys94@gmail.com>
2025-07-28 22:14:03 -0400

    chore: Update calendar booking link

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -167,7 +167,7 @@ export const ContactSection = () => {
                       us.
                     </p>
                     <a
-                      href="https://cal.com/shayanys/30min"
+                      href="https://cal.com/shayanys/lets-talk"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-11 px-8 w-full"
